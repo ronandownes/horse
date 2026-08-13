df8584e9a4a0f2272928b64c72f07be14fa1c65d
Ronan Downes <ronandownes@users.noreply.github.com>
2026-08-13 13:17:15 +0100

    Fix multipage route links

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
+const routeMap={
+  'coaching.html':'education.html',
+  'livery.html':'horse-care.html',
+  'events.html':'calendar.html',
+  'hire.html':'arena.html'
+};
+document.querySelectorAll('a[href]').forEach(link=>{
+  const href=link.getAttribute('href');
+  if(routeMap[href]) link.setAttribute('href',routeMap[href]);
+});
+
 const button=document.querySelector('.menu-button');
 const nav=document.querySelector('.topnav');
 if(button&&nav){
